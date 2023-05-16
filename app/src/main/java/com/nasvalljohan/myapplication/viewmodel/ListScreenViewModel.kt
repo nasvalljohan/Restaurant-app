54a2c15d76beb956c7dd2720c047a1f25875c272
nasvalljohan <j.nasvall95@gmail.com>
2023-05-16 23:33:04 +0200

    Refactor state name Add getFilters

--- app/src/main/java/com/nasvalljohan/myapplication/viewmodel/ListScreenViewModel.kt
+++ app/src/main/java/com/nasvalljohan/myapplication/viewmodel/ListScreenViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.nasvalljohan.myapplication.Constants
 import com.nasvalljohan.myapplication.ui.repository.GetAvailabilityUseCase
 import com.nasvalljohan.myapplication.ui.repository.GetFiltersUseCase
 import com.nasvalljohan.myapplication.ui.repository.GetRestaurantUseCase
@@ -16,32 +17,45 @@ class ListScreenViewModel(
     val getAvailabilityUseCase: GetAvailabilityUseCase,
 ) : ViewModel() {
 
-    private val _state = mutableStateOf(ListScreenState())
-    val state: State<ListScreenState> = _state
+    private val setState = mutableStateOf(ListScreenState())
+    val state: State<ListScreenState> = setState
 
     init {
         getRestaurants()
+        getFilters()
     }
-
-    var isPopUpOpen = mutableStateOf(false)
-        private set
 
     fun handleEvents(event: ListScreenEvent) {
         when (event) {
-            is ListScreenEvent.FilterEvent -> println(state.value.restaurants)
+            is ListScreenEvent.FilterEvent -> println("tjena")
             is ListScreenEvent.RestaurantSelectedEvent -> togglePopUp()
             ListScreenEvent.BackPress -> togglePopUp()
         }
     }
 
     private fun togglePopUp() {
-        isPopUpOpen.value = !isPopUpOpen.value
+        setState.value = ListScreenState(isPopUpOpen = !setState.value.isPopUpOpen)
     }
 
     private fun getRestaurants() {
         viewModelScope.launch {
             val response = getRestaurantUseCase()
-            _state.value = ListScreenState(restaurants = response.toList())
+            setState.value = ListScreenState(restaurants = response.toList())
+        }
+    }
+
+    private fun getFilters() {
+        viewModelScope.launch {
+            Constants.FILTERS.forEach {
+                setState.value =
+                    ListScreenState(filters = state.value.filters + getFiltersUseCase(it).toList())
+            }
+        }
+    }
+
+    private fun getAvailability() {
+        viewModelScope.launch {
+            TODO()
         }
     }
 }
