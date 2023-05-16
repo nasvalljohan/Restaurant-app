172f047bc7266a3b1558a05aa1c2d0c3c596fa7e
nasvalljohan <j.nasvall95@gmail.com>
2023-05-16 23:13:29 +0200

    Refactor name Add usecases and retrofit

--- app/src/main/java/com/nasvalljohan/myapplication/di/AppModule.kt
+++ app/src/main/java/com/nasvalljohan/myapplication/di/AppModule.kt
@@ -1,9 +1,35 @@
 package com.nasvalljohan.myapplication.di
 
+import com.nasvalljohan.myapplication.Constants.BASE_URL
+import com.nasvalljohan.myapplication.api.RestaurantAPI
+import com.nasvalljohan.myapplication.api.RestaurantRepoImpl
+import com.nasvalljohan.myapplication.api.RestaurantRepoInterface
+import com.nasvalljohan.myapplication.ui.repository.GetAvailabilityUseCase
+import com.nasvalljohan.myapplication.ui.repository.GetFiltersUseCase
+import com.nasvalljohan.myapplication.ui.repository.GetRestaurantUseCase
 import com.nasvalljohan.myapplication.viewmodel.ListScreenViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-val viewModel = module {
-    viewModel { ListScreenViewModel() }
+val AppModule = module {
+
+    single {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(RestaurantAPI::class.java)
+    }
+
+    single<RestaurantRepoInterface> { RestaurantRepoImpl(get()) }
+
+    single { GetRestaurantUseCase(get()) }
+
+    single { GetAvailabilityUseCase(get()) }
+
+    single { GetFiltersUseCase(get()) }
+
+    viewModel { ListScreenViewModel(get(), get(), get()) }
 }
