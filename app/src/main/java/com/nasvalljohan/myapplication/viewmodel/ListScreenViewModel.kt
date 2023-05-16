29c4bca610750d2f852a5a1e1756495a45aac1f2
nasvalljohan <j.nasvall95@gmail.com>
2023-05-16 23:14:02 +0200

    Initial call

--- app/src/main/java/com/nasvalljohan/myapplication/viewmodel/ListScreenViewModel.kt
+++ app/src/main/java/com/nasvalljohan/myapplication/viewmodel/ListScreenViewModel.kt
@@ -1,16 +1,34 @@
 package com.nasvalljohan.myapplication.viewmodel
 
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nasvalljohan.myapplication.ui.repository.GetAvailabilityUseCase
+import com.nasvalljohan.myapplication.ui.repository.GetFiltersUseCase
+import com.nasvalljohan.myapplication.ui.repository.GetRestaurantUseCase
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
 
-class ListScreenViewModel : ViewModel() {
+class ListScreenViewModel(
+    val getFiltersUseCase: GetFiltersUseCase,
+    val getRestaurantUseCase: GetRestaurantUseCase,
+    val getAvailabilityUseCase: GetAvailabilityUseCase,
+) : ViewModel() {
+
+    private val _state = mutableStateOf(ListScreenState())
+    val state: State<ListScreenState> = _state
+
+    init {
+        getRestaurants()
+    }
 
     var isPopUpOpen = mutableStateOf(false)
         private set
 
     fun handleEvents(event: ListScreenEvent) {
         when (event) {
-            is ListScreenEvent.FilterEvent -> println(event.filterId)
+            is ListScreenEvent.FilterEvent -> println(state.value.restaurants)
             is ListScreenEvent.RestaurantSelectedEvent -> togglePopUp()
             ListScreenEvent.BackPress -> togglePopUp()
         }
@@ -18,5 +36,12 @@ class ListScreenViewModel : ViewModel() {
 
     private fun togglePopUp() {
         isPopUpOpen.value = !isPopUpOpen.value
+    }
+
+    private fun getRestaurants() {
+        viewModelScope.launch {
+            val response = getRestaurantUseCase()
+            _state.value = ListScreenState(restaurants = response.toList())
+        }
     }
 }
