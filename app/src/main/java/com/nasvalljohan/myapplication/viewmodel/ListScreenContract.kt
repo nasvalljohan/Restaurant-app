02b0ad98bf2e5efc80a856de445b13f088b937dd
nasvalljohan <j.nasvall95@gmail.com>
2023-05-16 23:32:33 +0200

    Add state

--- app/src/main/java/com/nasvalljohan/myapplication/viewmodel/ListScreenContract.kt
+++ app/src/main/java/com/nasvalljohan/myapplication/viewmodel/ListScreenContract.kt
@@ -1,6 +1,6 @@
 package com.nasvalljohan.myapplication.viewmodel
 
-import com.nasvalljohan.myapplication.Constants.FILTERS
+import com.nasvalljohan.myapplication.ui.repository.model.Filter
 import com.nasvalljohan.myapplication.ui.repository.model.Restaurants
 
 sealed interface ListScreenEvent {
@@ -12,9 +12,5 @@ sealed interface ListScreenEvent {
 data class ListScreenState(
     val isPopUpOpen: Boolean = false,
     val restaurants: List<Restaurants> = emptyList(),
-    val filters: List<String> = FILTERS,
+    val filters: List<Filter> = emptyList(),
 )
-
-
-
-
