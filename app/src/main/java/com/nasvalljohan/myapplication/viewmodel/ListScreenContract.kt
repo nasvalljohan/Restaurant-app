aea06578647eaef1da11ac124983c0db74107b01
nasvalljohan <j.nasvall95@gmail.com>
2023-05-16 18:06:22 +0200

    Add new event

--- app/src/main/java/com/nasvalljohan/myapplication/viewmodel/ListScreenContract.kt
+++ app/src/main/java/com/nasvalljohan/myapplication/viewmodel/ListScreenContract.kt
@@ -3,4 +3,5 @@ package com.nasvalljohan.myapplication.viewmodel // ktlint-disable filename
 sealed interface ListScreenEvent {
     data class FilterEvent(val filterId: Int) : ListScreenEvent
     data class RestaurantSelectedEvent(val selectedRestaurant: Int) : ListScreenEvent
+    object BackPress : ListScreenEvent
 }
