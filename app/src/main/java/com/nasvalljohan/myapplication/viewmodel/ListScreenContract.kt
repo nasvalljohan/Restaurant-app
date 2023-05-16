0f86eb9484f50a012bc90a675692c54e368da0fd
nasvalljohan <j.nasvall95@gmail.com>
2023-05-16 23:12:28 +0200

    Add state

--- app/src/main/java/com/nasvalljohan/myapplication/viewmodel/ListScreenContract.kt
+++ app/src/main/java/com/nasvalljohan/myapplication/viewmodel/ListScreenContract.kt
@@ -1,7 +1,20 @@
-package com.nasvalljohan.myapplication.viewmodel // ktlint-disable filename
+package com.nasvalljohan.myapplication.viewmodel
+
+import com.nasvalljohan.myapplication.Constants.FILTERS
+import com.nasvalljohan.myapplication.ui.repository.model.Restaurants
 
 sealed interface ListScreenEvent {
     data class FilterEvent(val filterId: Int) : ListScreenEvent
     data class RestaurantSelectedEvent(val selectedRestaurant: Int) : ListScreenEvent
     object BackPress : ListScreenEvent
 }
+
+data class ListScreenState(
+    val isPopUpOpen: Boolean = false,
+    val restaurants: List<Restaurants> = emptyList(),
+    val filters: List<String> = FILTERS,
+)
+
+
+
+
