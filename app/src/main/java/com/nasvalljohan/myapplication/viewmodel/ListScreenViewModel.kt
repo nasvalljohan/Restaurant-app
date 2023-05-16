5586b165a6e6b87a7665a8e5abc3dcea45e589b7
nasvalljohan <j.nasvall95@gmail.com>
2023-05-16 15:09:49 +0200

    ref

--- app/src/main/java/com/nasvalljohan/myapplication/viewmodel/ListScreenViewModel.kt
+++ app/src/main/java/com/nasvalljohan/myapplication/viewmodel/ListScreenViewModel.kt
@@ -1,15 +1,27 @@
 package com.nasvalljohan.myapplication.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nasvalljohan.repository.RestaurantRepository
+import kotlinx.coroutines.launch
 
-class ListScreenViewModel() : ViewModel() {
+class ListScreenViewModel(
+    private val repo: RestaurantRepository,
+) : ViewModel() {
 
     fun handleEvents(event: ListScreenEvent) {
         when (event) {
             is ListScreenEvent.FilterEvent -> {
                 println(event.filterId)
+                getShit()
             }
             is ListScreenEvent.RestaurantSelectedEvent -> println(event.selectedRestaurant)
+        }
+    }
+
+    fun getShit() {
+        viewModelScope.launch {
+            val response = repo.getRestaurants().getOrNull()
         }
     }
 }
