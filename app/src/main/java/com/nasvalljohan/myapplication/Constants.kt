8a8ce6f0636cff5a30d00fc46004ab368358f85d
nasvalljohan <j.nasvall95@gmail.com>
2023-05-17 19:19:37 +0200

    Add map of filters

--- app/src/main/java/com/nasvalljohan/myapplication/Constants.kt
+++ app/src/main/java/com/nasvalljohan/myapplication/Constants.kt
@@ -9,4 +9,12 @@ object Constants {
         "23a38556-779e-4a3b-a75b-fcbc7a1c7a20",
         "0017e59c-4407-453f-a5be-901695708015",
     )
+
+    val filterToCategoryMap = mapOf(
+        "5c64dea3-a4ac-4151-a2e3-42e7919a925d" to "Top Rated",
+        "614fd642-3fa6-4f15-8786-dd3a8358cd78" to "Fast Food",
+        "c67cd8a3-f191-4083-ad28-741659f214d7" to "Take Out",
+        "23a38556-779e-4a3b-a75b-fcbc7a1c7a20" to "Fast Delivery",
+        "0017e59c-4407-453f-a5be-901695708015" to "Eat-in",
+    )
 }
