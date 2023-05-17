6360e2d077a99732873dadb1764984882d3757fb
nasvalljohan <j.nasvall95@gmail.com>
2023-05-17 10:35:06 +0200

    Add image to lazycolumn

--- app/src/main/java/com/nasvalljohan/myapplication/ui/listscreen/components/RestaurantList.kt
+++ app/src/main/java/com/nasvalljohan/myapplication/ui/listscreen/components/RestaurantList.kt
@@ -22,8 +22,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.nasvalljohan.myapplication.R
 import com.nasvalljohan.myapplication.ui.repository.model.Restaurant
 import com.nasvalljohan.myapplication.ui.theme.DarkText
@@ -67,10 +71,15 @@ private fun ListItem(
                 .clip(RoundedCornerShape(topEnd = 12.dp, topStart = 12.dp))
                 .clickable { onEvent(ListScreenEvent.RestaurantSelectedEvent(restaurant.id.toInt())) },
         ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Green),
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(restaurant.image_url)
+                    .crossfade(true)
+                    .build(),
+                placeholder = painterResource(R.drawable.placeholder),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize(),
             )
             Box(
                 modifier = Modifier
@@ -84,7 +93,7 @@ private fun ListItem(
                 ) {
                     TextTitle1(text = restaurant.name, color = DarkText)
                     if (restaurant.filterIds.isNotEmpty()) {
-                        TextSubtitle1(text = restaurant.filterIds[0], color = Subtitle)
+                        TextSubtitle1(text = "TODO", color = Subtitle)
                     }
                     Row(
                         horizontalArrangement = Arrangement.spacedBy(3.dp),
